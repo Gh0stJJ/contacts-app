02e8f1ddab50cb1d07168fff0f75d699b77d763f
Juan Valdiviezo <valdiviezojuan7@gmail.com>
2024-03-31 22:27:40 -0500

    html reuse

--- add.php
+++ add.php
@@ -30,99 +30,45 @@
 ?>
 
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset='utf-8'>
-  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-  <meta name='viewport' content='width=device-width, initial-scale=1'>
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.3/darkly/bootstrap.min.css" 
-  integrity="sha512-HDszXqSUU0om4Yj5dZOUNmtwXGWDa5ppESlX98yzbBS+z+3HQ8a/7kcdI1dv+jKq+1V5b01eYurE7+yFjw6Rdg==" 
-  crossorigin="anonymous" 
-  referrerpolicy="no-referrer" />
-  <script 
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
-    crossorigin="anonymous">
-  </script>
-  <!-- Static content -->
-  <link rel="stylesheet" href="./static/css/index.css">
-  <title>Contacts App</title> 
-</head>
-<body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand font-weight-bold" href="#">
-          <img class="mr-2" src="./static/img/logo.png" />
-          ContactsApp
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="add.php">Add Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-  </nav>
+<?php require './partials/header.php'; ?>
+<div class="container pt-5">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">Add New Contact</div>
+        <div class="card-body">
+          <?php if ($error): ?>
+            <p class="text-danger"><?= $error ?></p>
+              
+          <?php endif; ?>
 
-  <main>
-    <div class="container pt-5">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div class="card">
-            <div class="card-header">Add New Contact</div>
-            <div class="card-body">
-              <?php if ($error): ?>
-                <p class="text-danger"><?= $error ?></p>
-                  
-              <?php endif; ?>
+          <form method="post" action="add.php">
+            <div class="mb-3 row">
+              <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
 
-              <form method="post" action="add.php">
-                <div class="mb-3 row">
-                  <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
-    
-                  <div class="col-md-6">
-                    <input id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
-                  </div>
-                </div>
-    
-                <div class="mb-3 row">
-                  <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
-    
-                  <div class="col-md-6">
-                    <input id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
-                  </div>
-                </div>
-    
-                <div class="mb-3 row">
-                  <div class="col-md-6 offset-md-4">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                  </div>
-                </div>
-              </form>
+              <div class="col-md-6">
+                <input id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
+              </div>
             </div>
-          </div>
+
+            <div class="mb-3 row">
+              <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
+
+              <div class="col-md-6">
+                <input id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
+              </div>
+            </div>
+
+            <div class="mb-3 row">
+              <div class="col-md-6 offset-md-4">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
-    
-  </main>
+  </div>
+</div>
 
-</body>
-</html>
+<?php require './partials/footer.php'; ?>
---
+++ partials/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.3/darkly/bootstrap.min.css" 
+  integrity="sha512-HDszXqSUU0om4Yj5dZOUNmtwXGWDa5ppESlX98yzbBS+z+3HQ8a/7kcdI1dv+jKq+1V5b01eYurE7+yFjw6Rdg==" 
+  crossorigin="anonymous" 
+  referrerpolicy="no-referrer" />
+  <script 
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
+    crossorigin="anonymous">
+  </script>
+  <!-- Static content -->
+  <link rel="stylesheet" href="./static/css/index.css">
+  <title>Contacts App</title> 
+</head>
+<body>
+  <?php require 'navbar.php'; ?>
+
+  <main>
+
+  <!-- Content here -->
