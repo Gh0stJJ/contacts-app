f0be7de16e1fc65eb5dd9a335e505405b8c614f1
Juan Valdiviezo <valdiviezojuan7@gmail.com>
2024-05-21 08:39:40 -0500

    fix: DB port

--- database.php
+++ database.php
@@ -2,10 +2,12 @@
   $host = 'localhost';
   $user = 'root';
   $password = '';
+  $port = 3307;
   $database = 'contacts_app';
 
   try {
-    $connection = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+    $connection = new PDO("mysql:host=$host;port=$port;dbname=$database", $user, $password);
+    
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
   } catch (PDOException $e) {
